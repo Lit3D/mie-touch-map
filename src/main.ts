390fc024d125763bf94b12df3c4892b3fd4ee296
wisdman <wisdman@wisdman.io>
2020-07-13 18:39:35 +0500

    add multiple maps

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { SidebarComponent } from "./components"
 void async function main(){
 
   const sidebar = new SidebarComponent(".sidebar")
-  const sidebarCloseBtnNode = <HTMLButtonElement>document.querySelector("sidebar__close_btn")
+  const sidebarCloseBtnNode = <HTMLButtonElement>document.querySelector(".sidebar__close_btn")
   sidebarCloseBtnNode.addEventListener("click", () => sidebar.level = sidebar.level - 1)
 
 }()
